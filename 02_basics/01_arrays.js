1c6b388e98c6da5b3dd55f890f877f3fa0acfcba
Debashis Modak <your.email@example.com>
2025-07-21 20:41:00 +0530

    basic

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr);
+
+// Array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+console.log(myArr);
